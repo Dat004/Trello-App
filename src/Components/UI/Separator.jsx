b3ff66bbb442b165af33beed274e3bd7a8cc9f5b
Dat004 <alexanderdat98@gmail.com>
2025-09-18 10:17:35 +0700

    Building the important components ui: ScrollArea, Separator. Fix error missing return after '{' in the Avatar component

---
+++ src/Components/UI/Separator.jsx
@@ -0,0 +1,25 @@
+import { cn } from "@/lib/utils";
+import * as SeparatorPrimitive from "@radix-ui/react-separator";
+
+function Separator({
+  className,
+  orientation = "horizontal",
+  decorative = true,
+  ...props
+}) {
+  return (
+    <SeparatorPrimitive.Root
+      ref={ref}
+      decorative={decorative}
+      orientation={orientation}
+      className={cn(
+        "shrink-0 bg-border",
+        orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+export default Separator;

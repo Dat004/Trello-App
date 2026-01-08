6106e6dfb0c34d8cc0dad38be36d019456ed881c
Dat004 <alexanderdat98@gmail.com>
2026-01-08 23:30:12 +0700

    refactor(root): relocate AppInitializer and ToastProvider inside Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,48 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import AppInitializer from "./initializers/AppInitializer";
+import ToastProvider from "./providers/ToastProvider";
 import ProtectedRoute from "./routes/ProtectedRoute";
-import routes from "@/config/routes";
 import PublicRoute from "./routes/PublicRoute";
+import routes from "@/config/routes";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        {routes.map((route) => (
-          <Route
-            path={route.path}
-            element={
-              route.auth === "protected" ? (
-                <ProtectedRoute>
-                  {route.layout ? (
-                    <route.layout>
-                      <route.page />
-                    </route.layout>
+      <AppInitializer>
+        <ToastProvider>
+          <Routes>
+            {routes.map((route) => (
+              <Route
+                path={route.path}
+                element={
+                  route.auth === "protected" ? (
+                    <ProtectedRoute>
+                      {route.layout ? (
+                        <route.layout>
+                          <route.page />
+                        </route.layout>
+                      ) : (
+                        <route.page />
+                      )}
+                    </ProtectedRoute>
                   ) : (
-                    <route.page />
-                  )}
-                </ProtectedRoute>
-              ) : (
-                <PublicRoute>
-                  {route.layout ? (
-                    <route.layout>
-                      <route.page />
-                    </route.layout>
-                  ) : (
-                    <route.page />
-                  )}
-                </PublicRoute>
-              )
-            }
-          />
-        ))}
-      </Routes>
+                    <PublicRoute>
+                      {route.layout ? (
+                        <route.layout>
+                          <route.page />
+                        </route.layout>
+                      ) : (
+                        <route.page />
+                      )}
+                    </PublicRoute>
+                  )
+                }
+              />
+            ))}
+          </Routes>
+        </ToastProvider>
+      </AppInitializer>
     </Router>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import AppInitializer from "./initializers/AppInitializer";
-import ToastProvider from "./providers/ToastProvider";
 import App from "@/App";
 import "@/global.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AppInitializer>
-      <ToastProvider>
-        <App />
-      </ToastProvider>
-    </AppInitializer>
+    <App />
   </StrictMode>
 );

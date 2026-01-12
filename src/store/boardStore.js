efd89f80b47a699677a819554057b87f29308860
Dat004 <alexanderdat98@gmail.com>
2026-01-12 17:07:34 +0700

    refactor(store): remove currentBoard state

--- src/store/boardStore.js
+++ src/store/boardStore.js
@@ -2,7 +2,6 @@ import { create } from "zustand";
 
 const useBoardStore = create((set) => ({
   boards: [],
-  currentBoard: null,
   loading: true,
   isInitialized: false,
 
@@ -13,49 +12,26 @@ const useBoardStore = create((set) => ({
       isInitialized: true,
     })),
 
-  setCurrentBoard: (board) =>
-    set(() => ({
-      currentBoard: board,
-    })),
-
   addBoard: (board) =>
     set((state) => ({
       boards: [board, ...state.boards],
     })),
 
   updateBoard: (updatedData) =>
-    set((state) => {
-      // Update trong danh sách
-      const newBoards = state.boards.map((b) =>
-        b._id === updatedData._id
-          ? { ...b, ...updatedData } // Merge data
-          : b
-      );
-
-      // Update nếu board đang xem chính là board vừa sửa
-      let newCurrent = state.currentBoard;
-      if (state.currentBoard && state.currentBoard._id === updatedData._id) {
-        newCurrent = { ...state.currentBoard, ...updatedData }; // Merge giữ lại columns/cards
-      }
-
-      return {
-        boards: newBoards,
-        currentBoard: newCurrent,
-      };
-    }),
+    set((state) => ({
+      boards: state.boards.map((b) =>
+        b._id === updatedData._id ? { ...b, ...updatedData } : b
+      ),
+    })),
 
   removeBoard: (boardId) =>
     set((state) => ({
       boards: state.boards.filter((b) => b._id !== boardId),
-      // Nếu đang xem board bị xóa thì clear màn hình hoặc redirect
-      currentBoard:
-        state.currentBoard?._id === boardId ? null : state.currentBoard,
     })),
 
   clearBoards: () =>
     set(() => ({
       boards: [],
-      currentBoard: null,
       loading: false,
       isInitialized: false,
     })),

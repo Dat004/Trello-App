e48fc00cc7715f814f395121e08fcfa4ae3b7b22
Dat004 <alexanderdat98@gmail.com>
2026-01-16 10:30:56 +0700

    fix(api): correct comments API endpoint paths

--- src/api/comments.js
+++ src/api/comments.js
@@ -7,21 +7,21 @@ const cardsRoute = "/cards";
 export const commentsApi = {
     async getCommentByCardId(boardId, cardId) {
         try {
-            return await axiosClient.get(`${boardsRoute}/${boardId}/${cardsRoute}/${cardId}${commentsRoute}`);
+            return await axiosClient.get(`${boardsRoute}/${boardId}${cardsRoute}/${cardId}${commentsRoute}`);
         } catch (err) {
             return err.response;
         }
     },
     async addComment(boardId, cardId, data) {
         try {
-            return await axiosClient.post(`${boardsRoute}/${boardId}/${cardsRoute}/${cardId}${commentsRoute}/create`, data);
+            return await axiosClient.post(`${boardsRoute}/${boardId}${cardsRoute}/${cardId}${commentsRoute}/create`, data);
         } catch (err) {
             return err.response;
         }
     },
     async deleteComment(boardId, cardId, commentId, data) {
         try {
-            return await axiosClient.delete(`${boardsRoute}/${boardId}/${cardsRoute}/${cardId}${commentsRoute}/${commentId}`, {
+            return await axiosClient.delete(`${boardsRoute}/${boardId}${cardsRoute}/${cardId}${commentsRoute}/${commentId}`, {
                 data: data || {},
             });
         } catch (err) {

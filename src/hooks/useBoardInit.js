f520b9e7616c831db22563f979909292b7737ae0
Dat004 <alexanderdat98@gmail.com>
2026-01-26 21:44:36 +0700

    refactor(hooks): remove route-based conditional fetching in useBoardInit

--- src/hooks/useBoardInit.js
+++ src/hooks/useBoardInit.js
@@ -1,24 +1,15 @@
 import { useEffect } from "react";
-import { useLocation, matchPath } from "react-router-dom";
 
 import { useAuthStore, useBoardStore } from "@/store";
 import { boardApi } from "@/api/board";
-import paths from "@/config/paths";
 
 const useBoardInit = () => {
-  const location = useLocation();
-
   const user = useAuthStore((s) => s.user);
-  const isInitialized = useBoardStore((s) => s.isInitialized);
-
   const setBoards = useBoardStore((s) => s.setBoards);
   const clearBoards = useBoardStore((s) => s.clearBoards);
 
   useEffect(() => {
     if (!user) return;
-
-    const isBoardDetail = matchPath(paths.board, location.pathname);
-    if (isBoardDetail || isInitialized) return;
 
     const fetchBoards = async () => {
       const res = await boardApi.getMyBoards();
@@ -32,7 +23,7 @@ const useBoardInit = () => {
     };
 
     fetchBoards();
-  }, [location.pathname, user, isInitialized]);
+  }, [user]);
 };
 
 export default useBoardInit;
